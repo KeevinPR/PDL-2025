9456ba5df7e96954668f9d7fef6e14a78d662663
170174 <kevin.paniagua.romero@alumnos.upm.es>
2026-01-17 13:47:07 +0100

    if simple no puede llevar cualquier sentencia; un bloque es una sentencia compuesta (y “cualquier sentencia”)

--- ok2.js
+++ ok2.js
@@ -7,9 +7,9 @@ function int contarHasta(int limite) {
     contador = 0;
 
     for (i = 0; i == limite; i = i + 1) {
-        if (contador == 0) {
+        if (contador == 0) 
             contador = contador + 1;
-        }
+        
     }
 
     return contador;
